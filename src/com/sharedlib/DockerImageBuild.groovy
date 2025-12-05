dd23551af4ec6eb40af1f661582b0a7a776c1307
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-05 18:45:15 +0530

    Update DockerImageBuild.groovy

--- src/com/sharedlib/DockerImageBuild.groovy
+++ src/com/sharedlib/DockerImageBuild.groovy
@@ -1,1 +1,26 @@
+#!/usr/bin/env groovy
+package com.sharedlib
 
+class BuildDockerImage implements Serializable {
+	def script
+
+	BuildDockerImage(script) { this.script = script }
+
+	def buildDockerImage(String dockerImage) {
+
+    	script.echo "🔨 Building Docker Image: ${dockerImage}"
+
+    	def status = script.sh(
+        	script: "docker build . -t ${dockerImage}",
+        	returnStatus: true
+    	)
+
+    	if (status == 0) {
+        	script.echo "✔ Docker image '${dockerImage}' built successfully."
+        	return dockerImage
+    	} else {
+        	script.echo "❌ Docker build failed for image: ${dockerImage}"
+        	script.error("Stopping build because Docker image failed.")
+       	}
+	}
+}
