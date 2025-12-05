cac59fcca302cd9f7b5e4073e3be3482b23920fd
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-05 21:12:45 +0530

    Create JacocoCodeCoverage.groovy

---
+++ src/com/sharedlib/JacocoCodeCoverage.groovy
@@ -0,0 +1,32 @@
+#!/usr/bin/env groovy
+package com.sharedlib
+
+class JacocoCodeCoverage implements Serializable {
+	def script
+
+	JacocoCodeCoverage(script) { this.script = script }
+
+	def jacocoCodeCoverage(Map config = [:]) {
+
+		def required = ["JACOCO_GROUPID", "JACOCO_ARTIFACT_ID", "JACOCO_VERSION", "JACOCO_GOAL"]
+    	required.each { key ->
+        	if (!config[key] || config[key].trim() == "") {
+            	error "❌ JACOCO: Missing required parameter '${key}'"
+        	}
+    	}
+
+    	def jacocoCmd = "${config.JACOCO_GROUPID}:${config.JACOCO_ARTIFACT_ID}:${config.JACOCO_VERSION}:${config.JACOCO_GOAL}"
+
+    	script.echo "⏳ Running Jacoco step: ${jacocoCmd}..."
+
+    	try { sh """ 
+                 mvn ${jacocoCmd}
+                 mvn clean test jacoco:report
+             """ 
+        } catch (Exception ex) { error "❌ Jacoco Maven step failed: ${ex.message}" }
+
+    	echo "✔ JACOCO CODE COVERAGE completed Successfully"
+
+    	// sh 'mvn org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent'
+	}
+}
