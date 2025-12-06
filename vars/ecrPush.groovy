e8340c077bf211f34dd9a07d97ac4879dae0d224
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 07:42:40 +0530

    Update ecrPush.groovy

--- vars/ecrPush.groovy
+++ vars/ecrPush.groovy
@@ -1,19 +1,6 @@
 #!/usr/bin/env groovy
-import com.sharedlib.EcrPush
+import com.sharedlib.DockerPush
 
 def call(Map config = [:]) {
-
-   def required = ["DOCKER_IMAGE", "ECR_REPO_URI", "REGION", "AWS_CREDENTIALS_ID"]
-   required.each { key ->
-      if (!config[key] || config[key]?.trim() == "") {
-            error "❌ TRIVY: Missing required parameter '${key}'"
-      }
-   }
-   
-    return new EcrPush(this).ecrPush(
-        DOCKER_IMAGE:       config.DOCKER_IMAGE,
-        ECR_REPO_URI:       config.ECR_REPO_URI,
-        REGION:             config.REGION,
-        AWS_CREDENTIALS_ID: config.AWS_CREDENTIALS_ID
-    )
+   return new EcrPush(this).ecrPush(config)
 }
