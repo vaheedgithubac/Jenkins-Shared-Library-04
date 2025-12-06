7acfed7b3bd37ae6ff057b6783e819117cb16994
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 22:43:36 +0530

    Update PIPELINE_JAVA.groovy

--- vars/PIPELINE_JAVA.groovy
+++ vars/PIPELINE_JAVA.groovy
@@ -23,10 +23,7 @@ def call(Map config = [:]) {
 	   					//env.MY_GIT_LATEST_COMMIT_ID = getLatestCommitIdShort() ( To get this work, you should not declare a variable under pipeline environment{} block )
 						// MY_GIT_LATEST_COMMIT_ID = getLatestCommitIdShort()
 						if (config.EXECUTE_GITCHECKOUT_STAGE.equalsIgnoreCase('yes') {
-							env.MY_GIT_LATEST_COMMIT_ID = gitCheckout([
-															MY_GIT_URL:       config.MY_GIT_URL,
-															MY_GIT_REPO_TYPE: config.MY_GIT_REPO_TYPE
-															])
+							env.MY_GIT_LATEST_COMMIT_ID = gitCheckout([MY_GIT_URL: config.MY_GIT_URL, MY_GIT_REPO_TYPE: config.MY_GIT_REPO_TYPE])
 	   						echo "MY_GIT_LATEST_COMMIT_ID: ${env.MY_GIT_LATEST_COMMIT_ID}"
 						}
 					}
