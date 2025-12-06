ccc08fa2adf1a00ff7529866ab793383974c396a
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 19:43:49 +0530

    Update jacocoCodeCoverage.groovy

--- vars/jacocoCodeCoverage.groovy
+++ vars/jacocoCodeCoverage.groovy
@@ -2,8 +2,5 @@
 import com.sharedlib.JacocoCodeCoverage
 
 def call(Map config = [:]) {
-
-    return new JacocoCodeCoverage(this).jacocoCodeCoverage(config)
-
-   // The above step will return the latestCommitId from src/sharedlib/GitCheckout class
+    return new JacocoCodeCoverage(this).jacocoCodeCoverage(config) 
 }
