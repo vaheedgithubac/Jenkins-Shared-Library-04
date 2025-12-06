c9918f473be263b7a23f8264d0c748f80a14d359
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 08:21:28 +0530

    Update JacocoCodeCoverage.groovy

--- src/com/sharedlib/JacocoCodeCoverage.groovy
+++ src/com/sharedlib/JacocoCodeCoverage.groovy
@@ -11,22 +11,43 @@ class JacocoCodeCoverage implements Serializable {
 		def required = ["JACOCO_GROUPID", "JACOCO_ARTIFACT_ID", "JACOCO_VERSION", "JACOCO_GOAL"]
     	required.each { key ->
         	if (!config[key] || config[key].trim() == "") {
-            	error "❌ JACOCO: Missing required parameter '${key}'"
+            	script.error "❌ JACOCO: Missing required parameter '${key}'"
         	}
     	}
 
-    	def jacocoCmd = "${config.JACOCO_GROUPID}:${config.JACOCO_ARTIFACT_ID}:${config.JACOCO_VERSION}:${config.JACOCO_GOAL}"
+	    def jacoco_groupId     = config.JACOCO_GROUPID.trim()
+	    def jacoco_artifactId  = config.JACOCO_ARTIFACT_ID.trim()
+	    def jacoco_version     = config.JACOCO_VERSION.trim()
+	    def jacoco_goal        = config.JACOCO_GOAL.trim()
 
+	    script.echo "✔ JACOCO_GROUPID     = ${jacoco_groupId}"
+	    script.echo "✔ JACOCO_ARTIFACT_ID = ${jacoco_artifactId}"
+	    script.echo "✔ JACOCO_VERSION     = ${jacoco_version}"
+	    script.echo "✔ JACOCO_GOAL        = ${jacoco_goal}"
+
+    	def jacocoCmd = "${jacoco_groupId}:${jacoco_artifactId}:${jacoco_version}:${jacoco_goal}"
+
+        try {
+            def status = script.sh(returnStatus: true, 
+            					   script: """ mvn clean test ${jacocoCmd} jacoco:report """
+            					   )
+
+            if (status != 0) { script.error "❌ Jacoco Maven step failed (exit code: ${status})" }
+
+        } catch (Exception ex) { script.error "❌ Jacoco Maven step failed: ${ex.message}" }
+
+    	script.echo "✔ JACOCO CODE COVERAGE completed Successfully"
+
+
+    	/*
     	script.echo "⏳ Running Jacoco step: ${jacocoCmd}..."
-
-    	try { script.sh """ 
+        try { script.sh """ 
                  	mvn ${jacocoCmd}
                  	mvn clean test jacoco:report
               		""" 
-        } catch (Exception ex) { error "❌ Jacoco Maven step failed: ${ex.message}" }
+        } catch (Exception ex) { script.error "❌ Jacoco Maven step failed: ${ex.message}" }
+        */
 
-    	echo "✔ JACOCO CODE COVERAGE completed Successfully"
-
-    	// sh 'mvn org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent'
+    	// script.sh 'mvn org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent'
 	}
 }
