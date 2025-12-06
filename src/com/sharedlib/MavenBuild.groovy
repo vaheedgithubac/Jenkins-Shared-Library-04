5107cbd33203f6627b9b2cdf0450c0efd1783b5c
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 16:31:24 +0530

    Create MavenBuild.groovy

---
+++ src/com/sharedlib/MavenBuild.groovy
@@ -0,0 +1,44 @@
+#!/usr/bin/env groovy
+package com.sharedlib
+
+class MavenBuild implements Serializable {
+    def script
+
+    MavenBuild(script) { this.script = script }
+
+    def mavenBuild(Map config = [:]) {
+
+        def required = ["MAVEN_SKIP_TESTS"]
+        required.each { key ->
+            if (!config[key] || config[key].toString().trim() == "") {
+                script.error "❌ MAVEN BUILD: Missing required parameter '${key}'"
+            }
+        }
+                        
+        // Convert MAVEN_SKIP_TESTS to boolean safely
+        def mavenSkipTests = config.MAVEN_SKIP_TESTS?.toString()?.toBoolean() ?: false
+
+        // Accept MAVEN_GOALS or default
+        def mavenGoals = config.MAVEN_GOALS ?: "clean package"
+         
+        /*
+        def maven_skip_tests = ((config.MAVEN_SKIP_TESTS in [true, 'true', "true"]) ? 'true' : 'false') ?: 'true'
+        def maven_goals = config.maven_goals ?: 'clean package'
+        */
+
+        script.echo "🚀 Running Maven build"
+        script.echo "⚙️  Goals     : ${mavenGoals}"
+        script.echo "🧪 Skip tests: ${mavenSkipTests}"
+
+        
+        try {
+            script.sh """
+                mvn ${mavenGoals} -DskipTests=${mavenSkipTests}
+            """
+        } catch (Exception ex) {
+            script.error "❌ Maven Build failed: ${ex.message}"
+          }
+
+        script.echo "✔ Maven Build Completed Successfully"
+    }
+}
