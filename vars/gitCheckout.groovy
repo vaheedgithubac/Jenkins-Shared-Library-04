7acc2635cca81ec5c164338b84242622c5f91a79
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 19:43:04 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -2,8 +2,6 @@
 import com.sharedlib.GitCheckout
 
 def call(Map config = [:]) {
-
     return new GitCheckout(this).gitCheckout(config)
-
-   // The above step will return the latestCommitId from src/sharedlib/GitCheckout class
+    // The above step will return the latestCommitId from src/sharedlib/GitCheckout class
 }
