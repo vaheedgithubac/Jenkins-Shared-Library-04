6d49b97c0e5d39a180057c9d2bd5fe34b1e343b7
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 17:17:11 +0530

    Create SonarqubeQG.groovy

---
+++ src/com/sharedlib/SonarqubeQG.groovy
@@ -0,0 +1,26 @@
+#!/usr/bin/env groovy
+package com.sharedlib
+
+class SonarqubeQG implements Serializable {
+    def script
+
+    SonarqubeQG(script) { 
+        this.script = script 
+    }
+
+    def sonarqubeQG(Map config = [:]) {
+        // Default timeout in minutes
+        def timeoutMinutes = (config.TIMEOUT_MINUTES ?: 5).toInteger()
+
+        script.timeout(time: timeoutMinutes, unit: "MINUTES") {
+            def qg = script.waitForQualityGate(abortPipeline: false)
+
+            script.echo "🔹 SonarQube Quality Gate status: ${qg.status}"
+
+            /* Uncomment the following if you want to fail the pipeline on Quality Gate failure
+               if (qg.status != 'OK') {
+                   script.error "❌ Pipeline aborted due to Quality Gate failure: ${qg.status}"
+              } */
+        }
+    }
+}
