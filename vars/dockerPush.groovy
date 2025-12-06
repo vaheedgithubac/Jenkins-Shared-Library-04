0e8cb3540fcffe1dca7e4b9e132c21989d649293
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 09:09:05 +0530

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -2,17 +2,6 @@
 import com.sharedlib.DockerPush
 
 def call(Map config = [:]) {
-
-    def required = ["DOCKER_IMAGE", "DOCKER_HUB_CREDENTIALS_ID", "DOCKER_REPO_URI"]
-    required.each { key ->
-        if (!config[key]) {
-            error "❌ DOCKER REGISTRY: Missing required parameter '${key}'"
-        }
-    }
    
-    return new DockerPush(this).dockerPush(
-        DOCKER_IMAGE:              config.DOCKER_IMAGE,
-        DOCKER_HUB_CREDENTIALS_ID: config.DOCKER_HUB_CREDENTIALS_ID,
-        DOCKER_REPO_URI:           config.DOCKER_REPO_URI ?: "docker.io"
-    )
+    return new DockerPush(this).dockerPush(config)
 }
