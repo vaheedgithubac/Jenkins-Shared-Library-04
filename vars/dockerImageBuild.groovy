d67a3ae2aa086cbab97780104fd5c533f04795bb
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-06 08:37:36 +0530

    Update dockerImageBuild.groovy

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -2,17 +2,5 @@
 import com.sharedlib.DockerImageBuild
 
 def call(Map config = [:]) {
-	def required = ["PROJECT_NAME", "COMPONENT", "MY_GIT_LATEST_COMMIT_ID"]
-    required.each { key ->
-        if (!config[key]) {
-           error "❌ DOCKER IMAGE BUILD: Missing required parameter '${key}'"
-        }
-    }
-
-  def projectName   = config.PROJECT_NAME
-  def component     = config.COMPONENT
-  def imageTag      = config.MY_GIT_LATEST_COMMIT_ID
-
-  def dockerImage   = "${projectName}-${component}:${imageTag}"
-  return new DockerImageBuild(this).buildDockerImage(dockerImage)
+  return new DockerImageBuild(this).dockerImageBuild(config)
 }
