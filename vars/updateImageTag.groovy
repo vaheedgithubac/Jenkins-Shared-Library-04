fbe125f556b0fdbd1f24aea9f8a250960a0360c4
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-26 22:24:23 +0530

    Update updateImageTag.groovy

--- vars/updateImageTag.groovy
+++ vars/updateImageTag.groovy
@@ -2,5 +2,7 @@
 import com.sharedlib.UpdateImageTag
 
 def call(Map config = [:]) {
+  config.GIT_USER = env.GIT_USER
+  config.GIT_TOKEN = env.GIT_TOKEN
   return new UpdateImageTag(this).updateImageTag(config)
 }
