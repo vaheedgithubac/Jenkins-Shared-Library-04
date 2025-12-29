de5e0fd31b94ecbaa4aeb61cf0785cc99221ae9d
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-29 10:40:54 +0530

    Update UpdateImageTag.groovy

--- src/com/sharedlib/UpdateImageTag.groovy
+++ src/com/sharedlib/UpdateImageTag.groovy
@@ -122,7 +122,7 @@ class UpdateImageTag implements Serializable {
             )
         ]) {
             // def safeGitUrl = "https://${script.env.GIT_USER_SAFE}:${script.env.GIT_TOKEN_SAFE}@${vcsHost}/${script.env.GIT_USER_SAFE}/${config.GIT_REPO_NAME}.git"
-            def safeGitUrl = "https://$GIT_USER_SAFE@{vcsHost}/$GIT_USER_SAFE/${GIT_REPO_NAME}.git HEAD:${GIT_BRANCH_NAME}"
+            def safeGitUrl = "https://$GIT_USER_SAFE:$GIT_TOKEN_SAFE@{vcsHost}/$GIT_USER_SAFE/${GIT_REPO_NAME}.git HEAD:${GIT_BRANCH_NAME}"
             return script.sh(
                 script: "git push ${safeGitUrl} HEAD:${config.GIT_BRANCH_NAME}",
                 returnStatus: true
