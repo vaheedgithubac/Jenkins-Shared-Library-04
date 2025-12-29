015dc151ec423795f588d3cf386f4ddf1b148753
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-29 09:57:06 +0530

    Update updateImageTag.groovy

--- vars/updateImageTag.groovy
+++ vars/updateImageTag.groovy
@@ -2,13 +2,6 @@
 import com.sharedlib.UpdateImageTag
 
 def call(Map config = [:]) {
-    if (!env.GIT_USER || !env.GIT_TOKEN) {
-        error("❌ GIT_USER or GIT_TOKEN not set in environment")
-    }
-
-    config.GIT_USER  = env.GIT_USER
-    config.GIT_TOKEN = env.GIT_TOKEN
-
     new UpdateImageTag(this).updateImageTag(config)
 }
 
