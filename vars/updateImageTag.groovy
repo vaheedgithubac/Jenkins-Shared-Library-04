2cd4a26a09ddd7fe53cfca6b0ad34f566ed27def
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-30 13:21:32 +0530

    Update updateImageTag.groovy

--- vars/updateImageTag.groovy
+++ vars/updateImageTag.groovy
@@ -2,6 +2,27 @@
 import com.sharedlib.UpdateImageTag
 
 def call(Map config = [:]) {
-    new UpdateImageTag(this).updateImageTag(config)
+
+    def results = []
+    def updater = new UpdateImageTag(this)
+
+    if (config.TAGGED_DOCKER_IMAGE) {
+        def dockerConfig = config.clone()
+        dockerConfig.TAGGED_IMAGE = env.TAGGED_DOCKER_IMAGE
+        results << updater.updateImageTag(dockerConfig)
+    }
+
+    if (config.TAGGED_ECR_IMAGE) {
+        def ecrConfig = config.clone()
+        ecrConfig.TAGGED_IMAGE = env.TAGGED_ECR_IMAGE
+        results << updater.updateImageTag(ecrConfig)
+    }
+
+    if (results.isEmpty()) {
+        error "Neither TAGGED_DOCKER_IMAGE nor TAGGED_ECR_IMAGE was provided"
+    }
+
+    return results
 }
+
 
