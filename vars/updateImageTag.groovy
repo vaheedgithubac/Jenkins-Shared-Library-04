641c39a566956bd23151abf77829abf701d937ac
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-30 14:57:26 +0530

    Update updateImageTag.groovy

--- vars/updateImageTag.groovy
+++ vars/updateImageTag.groovy
@@ -6,23 +6,33 @@ def call(Map config = [:]) {
     def results = []
     def updater = new UpdateImageTag(this)
 
-    if (config.TAGGED_DOCKER_IMAGE) {
-        def dockerConfig = config.clone()
-        dockerConfig.TAGGED_IMAGE = env.TAGGED_DOCKER_IMAGE
-        results << updater.updateImageTag(dockerConfig)
+    if (!config.DEPLOYMENT_FILE && !config.HELM_VALUES_FILE) {
+            script.error("Neither DEPLOYMENT_FILE nor HELM_VALUES_FILE was provided, Please provide any one of them")
+        }
+
+    if (config.HELM_VALUES_FILE) {
+        if (!config.HELM_IMAGE_VERSION_KEY) { error "HELM_IMAGE_VERSION_KEY was not provided..."}
+
+        def helmConfig = config.clone()
+        results << updater.updateImageTag(helmConfig)
     }
 
-    if (config.TAGGED_ECR_IMAGE) {
-        def ecrConfig = config.clone()
-        ecrConfig.TAGGED_IMAGE = env.TAGGED_ECR_IMAGE
-        results << updater.updateImageTag(ecrConfig)
-    }
+    if (config.DEPLOYMENT_FILE) {
+        if (!config.TAGGED_DOCKER_IMAGE && !config.TAGGED_ECR_IMAGE) { error "Neither TAGGED_DOCKER_IMAGE nor TAGGED_ECR_IMAGE was provided..."}
 
-    if (results.isEmpty()) {
-        error "Neither TAGGED_DOCKER_IMAGE nor TAGGED_ECR_IMAGE was provided"
+        if (config.TAGGED_DOCKER_IMAGE) {
+            def dockerConfig = config.clone()
+            dockerConfig.TAGGED_IMAGE = env.TAGGED_DOCKER_IMAGE
+            results << updater.updateImageTag(dockerConfig)
+        }
+
+        if (config.TAGGED_ECR_IMAGE) {
+            def ecrConfig = config.clone()
+            ecrConfig.TAGGED_IMAGE = env.TAGGED_ECR_IMAGE
+            results << updater.updateImageTag(ecrConfig)
+        }
     }
 
     return results
 }
-
 
