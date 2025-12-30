7845c2b37089743ffde63dd48260124b81093e26
vaheedgithubac <53812220+vaheedgithubac@users.noreply.github.com>
2025-12-30 23:22:04 +0530

    Update updateImageTag.groovy

--- vars/updateImageTag.groovy
+++ vars/updateImageTag.groovy
@@ -6,35 +6,39 @@ def call(Map config = [:]) {
     def results = []
     def updater = new UpdateImageTag(this)
 
-    if (!config.DEPLOYMENT_FILE && !config.HELM_VALUES_FILE) {
-            script.error("Neither DEPLOYMENT_FILE nor HELM_VALUES_FILE was provided, Please provide any one of them")
+    if (!config.DEPLOYMENT_FILE && !config.HELM_VALUES_FILE) { error("Neither DEPLOYMENT_FILE nor HELM_VALUES_FILE provided...") }
+
+    def deploymentFilePath = "${env.WORKSPACE}/${config.DEPLOYMENT_FILE?.trim()}"   
+    def helmValuesFilePath = "${env.WORKSPACE}/${config.HELM_VALUES_FILE?.trim()}"
+
+    
+    if (fileExists(deploymentFilePath)) { 
+        echo "✅ Deployment file:${config.DEPLOYMENT_FILE} found at:${deploymentFilePath}" 
+
+        if (!config.TAGGED_DOCKER_IMAGE && !config.TAGGED_ECR_IMAGE) { error "Neither TAGGED_DOCKER_IMAGE nor TAGGED_ECR_IMAGE was provided..."}
+        
+        if (config.TAGGED_DOCKER_IMAGE) {
+            def dockerConfig = config.clone()
+            dockerConfig.TAGGED_IMAGE = config.TAGGED_DOCKER_IMAGE
+            results << updater.updateImageTag(dockerConfig)
         }
 
-    if (config.HELM_VALUES_FILE) {
+        if (config.TAGGED_ECR_IMAGE) {
+            def ecrConfig = config.clone()
+            ecrConfig.TAGGED_IMAGE = config.TAGGED_ECR_IMAGE
+            results << updater.updateImageTag(ecrConfig)
+        }
+    }
+
+
+    if (fileExists(helmValuesFilePath)) { 
+        echo "✅ Helm file:${config.HELM_VALUES_FILE} found at:${helmValuesFilePath}" 
+
         if (!config.HELM_IMAGE_VERSION_KEY) { error "HELM_IMAGE_VERSION_KEY was not provided..."}
 
         def helmConfig = config.clone()
         results << updater.updateImageTag(helmConfig)
     }
-
-    if (config.DEPLOYMENT_FILE) {
-        // if (!config.TAGGED_DOCKER_IMAGE && !config.TAGGED_ECR_IMAGE) { error "Neither TAGGED_DOCKER_IMAGE nor TAGGED_ECR_IMAGE was provided..."}
-        
-        if (!env.TAGGED_DOCKER_IMAGE && !env.TAGGED_ECR_IMAGE) { error "Neither TAGGED_DOCKER_IMAGE nor TAGGED_ECR_IMAGE was provided..."}
-        
-        if (env.TAGGED_DOCKER_IMAGE) {
-            def dockerConfig = config.clone()
-            dockerConfig.TAGGED_IMAGE = env.TAGGED_DOCKER_IMAGE
-            results << updater.updateImageTag(dockerConfig)
-        }
-
-        if (env.TAGGED_ECR_IMAGE) {
-            def ecrConfig = config.clone()
-            ecrConfig.TAGGED_IMAGE = env.TAGGED_ECR_IMAGE
-            results << updater.updateImageTag(ecrConfig)
-        }
-    }
-
+            
     return results
 }
-
